9664fbc84fe4aecfb3506f0397659c8e0d24233a
pamelaolney <pjolney12@gmail.com>
2016-08-20 18:46:16 -0400

    add functionality

--- angular_project3/js/categories/category.factory.js
+++ angular_project3/js/categories/category.factory.js
@@ -9,6 +9,6 @@
     ]);
 
     function CategoryFactoryFunction($resource){
-      return $resource("http://localhost:3000/");
+      return $resource("http://localhost:3000/categories");
     }
   }());
