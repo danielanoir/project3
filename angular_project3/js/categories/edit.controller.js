ef661b08bc314c00ae0a79b14c9db338a1bd6837
pamelaolney <pjolney12@gmail.com>
2016-08-22 13:56:01 -0400

    update and delete now works.  we have full crud for categories

--- angular_project3/js/categories/edit.controller.js
+++ angular_project3/js/categories/edit.controller.js
@@ -10,9 +10,14 @@
     ]);
 
     function CategoryEditControllerFunction( CategoryFactory, $stateParams ){
-      this.category = CategoryFactory.get({id: $stateParams.id});
-      this.update = function(){
-        this.category.$update({id: $stateParams.id});
+      var vm = this;
+      vm.category = CategoryFactory.get({id: $stateParams.id});
+      vm.update = function(){
+        vm.category.$update({id: $stateParams.id});
+      }
+      var vm = this;
+      vm.destroy = function(){
+        vm.category.$delete({id: $stateParams.id})
       }
     }
 
