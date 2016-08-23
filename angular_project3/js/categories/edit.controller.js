5bfd9750bb7dfae0410d975e7231194b52318629
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2016-08-22 22:53:56 -0400

    Progress with redirect, but still not there yet. Added alerts

--- angular_project3/js/categories/edit.controller.js
+++ angular_project3/js/categories/edit.controller.js
@@ -1,25 +1,29 @@
 "use strict";
 
-  (function(){
-    angular
-    .module("categories")
-    .controller("CategoryEditController", [
-      "CategoryFactory",
-      "$stateParams",
-      CategoryEditControllerFunction
-    ]);
+(function(){
+  angular
+  .module("categories")
+  .controller("CategoryEditController", [
+    "CategoryFactory",
+    "$stateParams",
+    CategoryEditControllerFunction
+  ]);
 
-    function CategoryEditControllerFunction( CategoryFactory, $stateParams ){
-      var vm = this;
-      vm.category = CategoryFactory.get({id: $stateParams.id});
-      vm.update = function(){
-        vm.category.$update({id: $stateParams.id});
-      }
-      var vm = this;
-      vm.destroy = function(){
-        vm.category.$delete({id: $stateParams.id})
-      }
+  function CategoryEditControllerFunction( CategoryFactory, $stateParams, $stateProvider ){
+    var vm = this
+    vm.category = CategoryFactory.get({id: $stateParams.id});
+    vm.update = function(){
+      vm.category.$update({id: $stateParams.id});
+      alert("You've successfully updated this Category!")
+      $state.go("categoryIndex" [stateParams] [location])
     }
+    var vm = this;
+    vm.destroy = function(){
+      vm.category.$delete({id: $stateParams.id})
+      alert("You've successfully deleted this Category!")
+      $state.go("categoryIndex" [stateParams] [location])
+    }
+  }
 
 
-  }());
+}());
--- angular_project3/js/categories/new.controller.js
+++ angular_project3/js/categories/new.controller.js
@@ -1,19 +1,20 @@
 "use strict";
 
-  (function(){
-    angular
-    .module("categories")
-    .controller("CategoryNewController", [
-      "CategoryFactory",
-      CategoryNewControllerFunction
-    ]);
+(function(){
+  angular
+  .module("categories")
+  .controller("CategoryNewController", [
+    "CategoryFactory",
+    CategoryNewControllerFunction
+  ]);
 
-    function CategoryNewControllerFunction(CategoryFactory){
-        var vm = this;
-        vm.category = new CategoryFactory();
-        vm.create = function(){
-          vm.category.$save();
-        }
+  function CategoryNewControllerFunction(CategoryFactory){
+    var vm = this;
+    vm.category = new CategoryFactory();
+    vm.create = function(){
+      vm.category.$save();
+      alert("You've successfully added this Category!")
     }
+  }
 
-  }());
+}());
