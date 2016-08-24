52b442a3d5d7fdac49ed2339211105abb5f9c010
pamelaolney <pjolney12@gmail.com>
2016-08-24 17:46:22 -0400

    update

--- angular_project3/js/categories/show.controller.js
+++ angular_project3/js/categories/show.controller.js
@@ -14,7 +14,7 @@
 
     function CategoryShowControllerFunction(CategoryFactory, LessonFactory, $stateParams, $http, $scope){
       var vm = this;
-      // vm.category = CategoryFactory.get({id: $stateParams.id});
+      vm.category = CategoryFactory.get({id: $stateParams.id});
       vm.lessons = LessonFactory.query({id: $stateParams.id});
       console.log($stateParams);
       console.log(vm.lessons);
