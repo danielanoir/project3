5f97c22efa2718b8869530fb2d6caf5c9caa6c11
pamelaolney <pjolney12@gmail.com>
2016-08-24 15:04:12 -0400

    nested resources is now functional

--- angular_project3/js/categories/category.factory.js
+++ angular_project3/js/categories/category.factory.js
@@ -1,31 +1,26 @@
 "use strict";
 
-  (function(){
-    angular
-    .module("categories")
-    .factory("CategoryFactory", [
-      "$resource",
-      CategoryFactoryFunction
-    ]);
+(function(){
+  angular
+  .module("categories")
+  .factory("CategoryFactory", [
+    "$resource",
+    CategoryFactoryFunction
+  ])
+  .factory("LessonFactory", [
+    "$resource",
+    LessonFactoryFunction
+  ]);
 
-    function CategoryFactoryFunction($resource){
-      return $resource("http://localhost:3000/categories/:id", {}, {
-        update: { method: "PUT" }
-      });
-    }
-  }());
+  function CategoryFactoryFunction($resource){
+    return $resource("http://localhost:3000/categories/:id", {}, {
+      update: { method: "PUT" }
+    });
+  }
 
-  (function(){
-    angular
-    .module("categories")
-    .factory("LessonFactory", [
-      "$resource",
-      LessonFactoryFunction
-    ]);
-
-    function LessonFactoryFunction($resource){
-      return $resource("http://localhost:3000/categories/:id/lessons/:id", {}, {
-        update: { method: "PUT" }
-      });
-    }
-  }());
+  function LessonFactoryFunction($resource){
+    return $resource("http://localhost:3000/categories/:id/lessons/", {}, {
+      update: { method: "PUT" }
+    });
+  }
+}());
--- angular_project3/js/categories/show.controller.js
+++ angular_project3/js/categories/show.controller.js
@@ -14,8 +14,9 @@
 
     function CategoryShowControllerFunction(CategoryFactory, LessonFactory, $stateParams, $http, $scope){
       var vm = this;
-      vm.category = CategoryFactory.get({id: $stateParams.id});
-      vm.lessons = LessonFactory.get({id: $stateParams.id});
+      // vm.category = CategoryFactory.get({id: $stateParams.id});
+      vm.lessons = LessonFactory.query({id: $stateParams.id});
+      console.log($stateParams);
       console.log(vm.lessons);
 
 
