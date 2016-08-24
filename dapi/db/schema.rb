6ca870f33c9b86697506b334f2a15be12b1280bc
Daniela Noir <daniela@danielanoir.com>
2016-08-24 10:22:13 -0400

    flips work with images but cant adjust css. Going to try a different way.

--- dapi/db/schema.rb
+++ dapi/db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 20160820160633) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
+    t.string   "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
