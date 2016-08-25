36166831efd8b9f36753b648325856bb059eab04
Daniela Noir <daniela@danielanoir.com>
2016-08-25 09:49:23 -0400

    updated returns in factory

--- js/categories/category.factory.js
+++ js/categories/category.factory.js
@@ -13,13 +13,13 @@
   ]);
 
   function CategoryFactoryFunction($resource){
-    return $resource("http://localhost:3000/categories/:id", {}, {
+    return $resource("https://dapi-app.herokuapp.com/categories/:id", {}, {
       update: { method: "PUT" }
     });
   }
 
   function LessonFactoryFunction($resource){
-    return $resource("http://localhost:3000/categories/:id/lessons/", {}, {
+    return $resource("https://dapi-app.herokuapp.com/categories/:id/lessons", {}, {
       update: { method: "PUT" }
     });
   }
